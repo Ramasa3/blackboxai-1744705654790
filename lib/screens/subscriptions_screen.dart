2208ef9f538c0b8d0a73afb4f2cea3fa10be6168
Ramasa3 <rama.sabbagh@student.hu.nl>
2025-04-15 10:27:47 +0200

    Add lib/screens/subscriptions_screen.dart 1744705666982

---
+++ lib/screens/subscriptions_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SubscriptionsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Subscriptions'),
+      ),
+      body: Center(
+        child: Text(
+          'Subscriptions Page - To be implemented',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
